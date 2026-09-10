bed5b2cc125e25f0be7c241e4d02ca7d6a0be37c
devjonathan1999-del <dev.jonathan1999@gmail.com>
2026-09-10 12:10:42 +0200

    test: register cave presentation scene contract

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -10,6 +10,7 @@ const TEST_SUITES := [
     preload("res://tests/test_save_system.gd"),
     preload("res://tests/test_terrain_visual_profile.gd"),
     preload("res://tests/test_ui_contract.gd"),
+    preload("res://tests/test_scene_contract.gd"),
     preload("res://tests/test_vertical_slice_acceptance.gd"),
 ]
 
